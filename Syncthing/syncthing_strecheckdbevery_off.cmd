6fa9fa5a4eabf727e496fcc1ca8c2ce612ac0977
Catfriend1 <>
2021-04-06 12:36:06 +0200

    Fix syncthing_strecheckdbevery scripts

--- Syncthing/syncthing_strecheckdbevery_off.cmd
+++ Syncthing/syncthing_strecheckdbevery_off.cmd
@@ -3,3 +3,4 @@ REM
 REG DELETE "HKLM\System\CurrentControlSet\Services\Syncthing\Parameters" /v "AppEnvironmentExtra" /f
 REM 
 timeout 3
+goto :eof
--- Syncthing/syncthing_strecheckdbevery_on.cmd
+++ Syncthing/syncthing_strecheckdbevery_on.cmd
@@ -1,5 +1,6 @@
 @echo off
 REM 
-REG ADD "HKLM\System\CurrentControlSet\Services\Syncthing\Parameters" /v "AppEnvironmentExtra" /t REG_MULTI_SZ /d "STRECHECKDBEVERY=0\0" /f
+REG ADD "HKLM\System\CurrentControlSet\Services\Syncthing\Parameters" /v "AppEnvironmentExtra" /t REG_MULTI_SZ /d "STRECHECKDBEVERY=1s\0" /f
 REM 
 timeout 3
+goto :eof
