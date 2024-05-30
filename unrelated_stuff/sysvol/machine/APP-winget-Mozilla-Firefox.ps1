67af9d276da92d55f11d17f36dd62dfe0b827d31
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2024-05-30 22:50:27 +0200

    Sysvol/machine: Mozilla Firefox

---
+++ unrelated_stuff/sysvol/machine/APP-winget-Mozilla-Firefox.ps1
@@ -0,0 +1,11 @@
+﻿# Runtime Variables.
+$wingetExe = (Resolve-Path -ErrorAction Stop -Path (${ENV:ProgramFiles} + "\WindowsApps\Microsoft.DesktopAppInstaller_*\winget.exe") | Select -First 1).Path
+#
+& $wingetExe install --accept-source-agreements --source winget --exact --id "Mozilla.Firefox" --scope machine
+#
+attrib +s +h "${ENV:PUBLIC}\Desktop\Firefox.lnk"
+#
+# Disable Services
+Set-Service -Name "MozillaMaintenance" -StartupType Disabled -Status Stopped
+#
+Exit 0
